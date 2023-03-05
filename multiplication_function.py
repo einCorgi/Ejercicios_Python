333cb5185e1f5613420c6e017393116567b9369e
BloodyCorgi <benjamingalindodelarosa@gmail.com>
2023-03-04 22:24:49 -0600

    Multiplication function solved

---
+++ multiplication_function.py
@@ -0,0 +1,9 @@
+def multiplication(*args):
+    result = 1
+    for i in args:
+        result *= i
+    return result  # I must put attention to this detail, the return function...
+    # The identation IS A MUST when trying to get the result we wanted ;(
+
+
+print(multiplication(2, 3, 4, 5, 6))
