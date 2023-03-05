ea9fce0a0f56792e7c114d7123f607d44dea0907
BloodyCorgi <benjamingalindodelarosa@gmail.com>
2023-03-05 13:13:05 -0600

    Comments on code

--- kwargs_key_value_function.py
+++ kwargs_key_value_function.py
@@ -1,6 +1,8 @@
+# Let´s define our function
 def listingTerms(**kwargs):  # kwargs can accept key-value (dictionary) terms
-    for key, value in kwargs.items():  #
-        print(f"{key}:{value}")
+    # Let´s i-terate every element in our kwargs argument
+    for key, value in kwargs.items():  # We use the .items method in order to return a new view of the Dictionary Items
+        print(f"{key}:{value}")  # Let's print our f-string output
 
 
 listingTerms(IDE="Integrated Development Environment", API="Application Program Interface")
