592a9aca5f091e60dbf0d341e53d8ee5db89e9b8
BloodyCorgi <benjamingalindodelarosa@gmail.com>
2023-03-05 23:13:33 -0600

    More recursive functions

--- Recursive_functions.py
+++ Recursive_functions.py
@@ -13,5 +13,25 @@ def factorial(numero):
         return numero * factorial(numero - 1)
 
 
+def Descendiente(numero):
+    if numero == 1:
+        print(1)
+    else:
+        print(numero)
+        return Descendiente(numero-1)
+
+
+def SegundoDescendiente(numero):
+    if numero == 1:
+        print(1)
+    elif numero <= 0:
+        print("valor incorrecto")
+    else:
+        print(numero)
+        return SegundoDescendiente(numero-1)
+
+
 print(recursiveFunction(5))
 print(factorial(5))
+Descendiente(5)
+SegundoDescendiente(7)
